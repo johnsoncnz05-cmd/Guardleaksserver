f8132b23b5570f5a6311d866c9e1fec336a4adba
johnsoncnz05-cmd <johnsoncnz05@gmail.com>
2025-10-16 07:51:34 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,18 @@ import { fileURLToPath } from "url";
 import express from "express";
 import dotenv from "dotenv";
 import { requireAuth } from "./middleware/auth.js";
+import cors from 'cors';
+
+app.use(cors({
+  origin: [
+    'https://guardleaks.com',
+    'https://www.guardleaks.com',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 // after dynamic imports block
 
 
