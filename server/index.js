d9efc5a44491447307bf4f1055d7d603a890ed30
johnsoncnz05-cmd <johnsoncnz05@gmail.com>
2025-10-16 13:36:44 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,6 +37,7 @@ app.use(
       "https://guardleaks.com",
       "https://www.guardleaks.com",
       "http://localhost:5173",
+      "http://localhost:8081",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
